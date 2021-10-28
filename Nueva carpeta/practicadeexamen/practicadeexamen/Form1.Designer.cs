c582ab0e0e72a3b1fb184239e4a2cfe6b65795cd
cesar jafet vivas solis <88453459+cesar11b@users.noreply.github.com>
2021-10-28 00:55:39 -0600

    Add files via upload

---
+++ Nueva carpeta/practicadeexamen/practicadeexamen/Form1.Designer.cs
@@ -0,0 +1,348 @@
+﻿
+namespace practicadeexamen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblimpr = new System.Windows.Forms.Label();
+            this.txtIng = new System.Windows.Forms.TextBox();
+            this.btnHola = new System.Windows.Forms.Button();
+            this.btnAdios = new System.Windows.Forms.Button();
+            this.btnSaludo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMandarSaludo = new System.Windows.Forms.Label();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.btnResta = new System.Windows.Forms.Button();
+            this.btnDivision = new System.Windows.Forms.Button();
+            this.btnMultiplicacion = new System.Windows.Forms.Button();
+            this.lblRespuestaCalculable = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbtMenorEdad = new System.Windows.Forms.RadioButton();
+            this.rbtAdulto = new System.Windows.Forms.RadioButton();
+            this.rbtTerceraEdad = new System.Windows.Forms.RadioButton();
+            this.lblBanderaBotonUsado = new System.Windows.Forms.Label();
+            this.btnMostrado = new System.Windows.Forms.Button();
+            this.btnAbrirForm2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblimpr
+            // 
+            this.lblimpr.AutoSize = true;
+            this.lblimpr.Location = new System.Drawing.Point(13, 13);
+            this.lblimpr.Name = "lblimpr";
+            this.lblimpr.Size = new System.Drawing.Size(0, 13);
+            this.lblimpr.TabIndex = 0;
+            // 
+            // txtIng
+            // 
+            this.txtIng.Location = new System.Drawing.Point(468, 13);
+            this.txtIng.Name = "txtIng";
+            this.txtIng.Size = new System.Drawing.Size(100, 20);
+            this.txtIng.TabIndex = 1;
+            // 
+            // btnHola
+            // 
+            this.btnHola.Location = new System.Drawing.Point(213, 2);
+            this.btnHola.Name = "btnHola";
+            this.btnHola.Size = new System.Drawing.Size(75, 23);
+            this.btnHola.TabIndex = 2;
+            this.btnHola.Text = "Hola";
+            this.btnHola.UseVisualStyleBackColor = true;
+            this.btnHola.Click += new System.EventHandler(this.btnHola_Click);
+            // 
+            // btnAdios
+            // 
+            this.btnAdios.Location = new System.Drawing.Point(213, 45);
+            this.btnAdios.Name = "btnAdios";
+            this.btnAdios.Size = new System.Drawing.Size(75, 23);
+            this.btnAdios.TabIndex = 3;
+            this.btnAdios.Text = "Adios";
+            this.btnAdios.UseVisualStyleBackColor = true;
+            this.btnAdios.Click += new System.EventHandler(this.btnAdios_Click);
+            // 
+            // btnSaludo
+            // 
+            this.btnSaludo.Location = new System.Drawing.Point(637, 108);
+            this.btnSaludo.Name = "btnSaludo";
+            this.btnSaludo.Size = new System.Drawing.Size(75, 23);
+            this.btnSaludo.TabIndex = 4;
+            this.btnSaludo.Text = "Saludo";
+            this.btnSaludo.UseVisualStyleBackColor = true;
+            this.btnSaludo.Click += new System.EventHandler(this.btnSaludo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(353, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ingrese su nombre:";
+            // 
+            // lblMandarSaludo
+            // 
+            this.lblMandarSaludo.AutoSize = true;
+            this.lblMandarSaludo.Location = new System.Drawing.Point(356, 54);
+            this.lblMandarSaludo.Name = "lblMandarSaludo";
+            this.lblMandarSaludo.Size = new System.Drawing.Size(0, 13);
+            this.lblMandarSaludo.TabIndex = 6;
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(13, 150);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(52, 13);
+            this.lblA.TabIndex = 7;
+            this.lblA.Text = "Ingrese A";
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(13, 193);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(52, 13);
+            this.lblB.TabIndex = 8;
+            this.lblB.Text = "Ingrese B";
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.Location = new System.Drawing.Point(205, 148);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(83, 23);
+            this.btnSuma.TabIndex = 9;
+            this.btnSuma.Text = "Suma";
+            this.btnSuma.UseVisualStyleBackColor = true;
+            this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click);
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(75, 150);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 10;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(75, 193);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 11;
+            // 
+            // btnResta
+            // 
+            this.btnResta.Location = new System.Drawing.Point(205, 193);
+            this.btnResta.Name = "btnResta";
+            this.btnResta.Size = new System.Drawing.Size(83, 23);
+            this.btnResta.TabIndex = 12;
+            this.btnResta.Text = "Resta";
+            this.btnResta.UseVisualStyleBackColor = true;
+            this.btnResta.Click += new System.EventHandler(this.btnResta_Click);
+            // 
+            // btnDivision
+            // 
+            this.btnDivision.Location = new System.Drawing.Point(304, 193);
+            this.btnDivision.Name = "btnDivision";
+            this.btnDivision.Size = new System.Drawing.Size(83, 23);
+            this.btnDivision.TabIndex = 14;
+            this.btnDivision.Text = "Division";
+            this.btnDivision.UseVisualStyleBackColor = true;
+            this.btnDivision.Click += new System.EventHandler(this.btnDivision_Click);
+            // 
+            // btnMultiplicacion
+            // 
+            this.btnMultiplicacion.Location = new System.Drawing.Point(304, 148);
+            this.btnMultiplicacion.Name = "btnMultiplicacion";
+            this.btnMultiplicacion.Size = new System.Drawing.Size(83, 23);
+            this.btnMultiplicacion.TabIndex = 13;
+            this.btnMultiplicacion.Text = "Multiplicacion";
+            this.btnMultiplicacion.UseVisualStyleBackColor = true;
+            this.btnMultiplicacion.Click += new System.EventHandler(this.btnMultiplicacion_Click);
+            // 
+            // lblRespuestaCalculable
+            // 
+            this.lblRespuestaCalculable.AutoSize = true;
+            this.lblRespuestaCalculable.Location = new System.Drawing.Point(75, 249);
+            this.lblRespuestaCalculable.Name = "lblRespuestaCalculable";
+            this.lblRespuestaCalculable.Size = new System.Drawing.Size(35, 13);
+            this.lblRespuestaCalculable.TabIndex = 15;
+            this.lblRespuestaCalculable.Text = "label2";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(75, 277);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(35, 13);
+            this.lbl2.TabIndex = 16;
+            this.lbl2.Text = "label2";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbtTerceraEdad);
+            this.groupBox1.Controls.Add(this.rbtAdulto);
+            this.groupBox1.Controls.Add(this.rbtMenorEdad);
+            this.groupBox1.Location = new System.Drawing.Point(468, 177);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 155);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // rbtMenorEdad
+            // 
+            this.rbtMenorEdad.AutoSize = true;
+            this.rbtMenorEdad.Checked = true;
+            this.rbtMenorEdad.Location = new System.Drawing.Point(7, 21);
+            this.rbtMenorEdad.Name = "rbtMenorEdad";
+            this.rbtMenorEdad.Size = new System.Drawing.Size(98, 17);
+            this.rbtMenorEdad.TabIndex = 0;
+            this.rbtMenorEdad.TabStop = true;
+            this.rbtMenorEdad.Text = "Menor de Edad";
+            this.rbtMenorEdad.UseVisualStyleBackColor = true;
+            // 
+            // rbtAdulto
+            // 
+            this.rbtAdulto.AutoSize = true;
+            this.rbtAdulto.Location = new System.Drawing.Point(7, 67);
+            this.rbtAdulto.Name = "rbtAdulto";
+            this.rbtAdulto.Size = new System.Drawing.Size(55, 17);
+            this.rbtAdulto.TabIndex = 1;
+            this.rbtAdulto.Text = "Adulto";
+            this.rbtAdulto.UseVisualStyleBackColor = true;
+            // 
+            // rbtTerceraEdad
+            // 
+            this.rbtTerceraEdad.AutoSize = true;
+            this.rbtTerceraEdad.Location = new System.Drawing.Point(7, 115);
+            this.rbtTerceraEdad.Name = "rbtTerceraEdad";
+            this.rbtTerceraEdad.Size = new System.Drawing.Size(64, 17);
+            this.rbtTerceraEdad.TabIndex = 2;
+            this.rbtTerceraEdad.Text = "Anciano";
+            this.rbtTerceraEdad.UseVisualStyleBackColor = true;
+            // 
+            // lblBanderaBotonUsado
+            // 
+            this.lblBanderaBotonUsado.AutoSize = true;
+            this.lblBanderaBotonUsado.Location = new System.Drawing.Point(189, 277);
+            this.lblBanderaBotonUsado.Name = "lblBanderaBotonUsado";
+            this.lblBanderaBotonUsado.Size = new System.Drawing.Size(35, 13);
+            this.lblBanderaBotonUsado.TabIndex = 18;
+            this.lblBanderaBotonUsado.Text = "label2";
+            // 
+            // btnMostrado
+            // 
+            this.btnMostrado.Location = new System.Drawing.Point(375, 277);
+            this.btnMostrado.Name = "btnMostrado";
+            this.btnMostrado.Size = new System.Drawing.Size(75, 23);
+            this.btnMostrado.TabIndex = 19;
+            this.btnMostrado.Text = "mostrar";
+            this.btnMostrado.UseVisualStyleBackColor = true;
+            this.btnMostrado.Click += new System.EventHandler(this.btnMostrado_Click);
+            // 
+            // btnAbrirForm2
+            // 
+            this.btnAbrirForm2.Location = new System.Drawing.Point(674, 277);
+            this.btnAbrirForm2.Name = "btnAbrirForm2";
+            this.btnAbrirForm2.Size = new System.Drawing.Size(75, 23);
+            this.btnAbrirForm2.TabIndex = 20;
+            this.btnAbrirForm2.Text = "Abrir form2";
+            this.btnAbrirForm2.UseVisualStyleBackColor = true;
+            this.btnAbrirForm2.Click += new System.EventHandler(this.btnAbrirForm2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAbrirForm2);
+            this.Controls.Add(this.btnMostrado);
+            this.Controls.Add(this.lblBanderaBotonUsado);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lblRespuestaCalculable);
+            this.Controls.Add(this.btnDivision);
+            this.Controls.Add(this.btnMultiplicacion);
+            this.Controls.Add(this.btnResta);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.btnSuma);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.lblMandarSaludo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSaludo);
+            this.Controls.Add(this.btnAdios);
+            this.Controls.Add(this.btnHola);
+            this.Controls.Add(this.txtIng);
+            this.Controls.Add(this.lblimpr);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblimpr;
+        private System.Windows.Forms.TextBox txtIng;
+        private System.Windows.Forms.Button btnHola;
+        private System.Windows.Forms.Button btnAdios;
+        private System.Windows.Forms.Button btnSaludo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMandarSaludo;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Button btnResta;
+        private System.Windows.Forms.Button btnDivision;
+        private System.Windows.Forms.Button btnMultiplicacion;
+        private System.Windows.Forms.Label lblRespuestaCalculable;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbtTerceraEdad;
+        private System.Windows.Forms.RadioButton rbtAdulto;
+        private System.Windows.Forms.RadioButton rbtMenorEdad;
+        private System.Windows.Forms.Label lblBanderaBotonUsado;
+        private System.Windows.Forms.Button btnMostrado;
+        private System.Windows.Forms.Button btnAbrirForm2;
+    }
+}
+
---
+++ Nueva carpeta/practicadeexamen/practicadeexamen/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practicadeexamen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHola_Click(object sender, EventArgs e)
+        {
+            lblimpr.Text = "haif aiiaaa!!!";
+        }
+
+        private void btnAdios_Click(object sender, EventArgs e)
+        {
+            lblMandarSaludo.Text = "aaaaaaaaaaa";
+            this.Text = "ashica wata?";
+        }
+
+        private void btnSaludo_Click(object sender, EventArgs e)
+        {
+            string v = "";
+            string tipoedad = "";
+            string edad = "";
+
+            if (rbtMenorEdad.Checked == true)
+            {
+                edad = rbtMenorEdad.Text;
+
+
+            }
+
+            if (rbtTerceraEdad.Checked == true)
+            {
+                edad = rbtTerceraEdad.Text;
+
+
+            }
+
+            if (rbtAdulto.Checked == true)
+            {
+                edad = rbtAdulto.Text;
+
+
+            }
+
+           
+
+
+            if (txtIng.Text == v)
+                {
+
+                    lblMandarSaludo.Text="Porfavor introduzca el nombre en la casilla correspondiente";
+                   
+                }
+                else
+                {
+                    lblMandarSaludo.Text = "hola, " + txtIng.Text + " es tu nombre, ehh? y eres un "+edad;
+
+
+
+
+                    
+
+                }
+
+
+
+          
+                
+
+                   
+            
+
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblBanderaBotonUsado.Text = "";
+            lblimpr.Text = "";
+            lblRespuestaCalculable.Text = "";
+        }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(txtA.Text);
+
+            double b = Convert.ToDouble(txtB.Text);
+
+
+            double resultado = a + b;
+
+            lblRespuestaCalculable.Text = "La suma de los valores ingresados " + a + " y " + b + " es: " 
+                + resultado;
+
+            lbl2.Text = resultado.ToString();
+            txtA.Text = resultado.ToString();
+
+
+        }
+
+        private void btnResta_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(txtA.Text);
+
+            double b = Convert.ToDouble(txtB.Text);
+
+
+            double resultado = a - b;
+
+            lblRespuestaCalculable.Text = "La resta de los valores ingresados " + a + " y " + b + " es: "
+                + resultado;
+
+            lbl2.Text = resultado.ToString();
+        }
+
+        private void btnMultiplicacion_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(txtA.Text);
+
+            double b = Convert.ToDouble(txtB.Text);
+
+
+            double resultado = a * b;
+
+            lblRespuestaCalculable.Text = "La multiplicacion de los valores ingresados " + a + " y " + b + " es: "
+                + resultado;
+
+            lbl2.Text = resultado.ToString();
+        }
+
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(txtA.Text);
+
+            double b = Convert.ToDouble(txtB.Text);
+
+
+            double resultado = a / b;
+
+            lblRespuestaCalculable.Text = "La division de los valores ingresados " + a + " y " + b + " es: "
+                + resultado;
+
+            lbl2.Text = resultado.ToString();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnMostrado_Click(object sender, EventArgs e)
+        {
+            
+
+            DialogResult señal= MessageBox.Show("presione un boton para probar", "muestra de boton", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+
+            if(señal == DialogResult.Yes)
+            {
+
+                lblBanderaBotonUsado.Text = "seleccionaste el boton ´Si´";
+
+
+
+            }
+
+
+            if (señal == DialogResult.No)
+            {
+
+                lblBanderaBotonUsado.Text = "seleccionaste el boton ´No´";
+
+
+
+            }
+
+
+
+
+        }
+
+        private void btnAbrirForm2_Click(object sender, EventArgs e)
+        {
+            Form2 CambioForm = new Form2();
+
+            CambioForm.ShowDialog();
+        }
+    }
+}
